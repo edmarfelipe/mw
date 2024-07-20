fb14b1564ff386a27995d0b3efab0858a8cc6b16
Edmar Felipe <edmarfelipe@outlook.com>
2024-07-20 11:58:32 -0300

    feat: add middleware interface

---
+++ chain.go
@@ -0,0 +1,16 @@
+package mw
+
+import "net/http"
+
+// Middleware is a type that wraps an http.Handler
+type Middleware interface {
+	Handle(http.Handler) http.HandlerFunc
+}
+
+// Chain is a function that chains multiple Middleware
+func Chain(f http.Handler, mws ...Middleware) http.HandlerFunc {
+	for _, m := range mws {
+		f = m.Handle(f)
+	}
+	return f.ServeHTTP
+}
